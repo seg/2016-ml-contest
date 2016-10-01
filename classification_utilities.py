52b232cdeb519a591f4b4f94e3320d2afa566155
Matt Hall <matt@agilegeoscience.com>
2016-10-01 10:05:06 +0900

    ready for binder

--- classification_utilities.py
+++ classification_utilities.py
@@ -12,42 +12,50 @@ def display_cm(cm, labels, hide_zeros=False,
     recall = np.diagonal(cm)/cm.sum(axis=1).astype('float')
     F1 = 2 * (precision * recall) / (precision + recall)
     
+    precision[np.isnan(precision)] = 0
+    recall[np.isnan(recall)] = 0
+    F1[np.isnan(F1)] = 0
+    
+    total_precision = np.sum(precision * cm.sum(axis=1)) / cm.sum(axis=(0,1))
+    total_recall = np.sum(recall * cm.sum(axis=1)) / cm.sum(axis=(0,1))
+    total_F1 = np.sum(F1 * cm.sum(axis=1)) / cm.sum(axis=(0,1))
+    #print total_precision
+    
     columnwidth = max([len(x) for x in labels]+[5]) # 5 is value length
     empty_cell = " " * columnwidth
     # Print header
-    print "    " + " Pred",
+    print("    " + " Pred", end=' ')
     for label in labels: 
-        print "%{0}s".format(columnwidth) % label,
-    print
-    print "    " + " True"
+        print("%{0}s".format(columnwidth) % label, end=' ')
+    print("%{0}s".format(columnwidth) % 'Total')
+    print("    " + " True")
     # Print rows
     for i, label1 in enumerate(labels):
-        print "    %{0}s".format(columnwidth) % label1,
+        print("    %{0}s".format(columnwidth) % label1, end=' ')
         for j in range(len(labels)): 
             cell = "%{0}d".format(columnwidth) % cm[i, j]
             if hide_zeros:
                 cell = cell if float(cm[i, j]) != 0 else empty_cell
-            print cell,
-        
-        print
+            print(cell, end=' ')
+        print("%{0}d".format(columnwidth) % sum(cm[i,:]))
         
     if display_metrics:
-        print
-        print "Precision",
+        print()
+        print("Precision", end=' ')
         for j in range(len(labels)):
             cell = "%{0}.2f".format(columnwidth) % precision[j]
-            print cell,
-        print
-        print "   Recall",
+            print(cell, end=' ')
+        print("%{0}.2f".format(columnwidth) % total_precision)
+        print("   Recall", end=' ')
         for j in range(len(labels)):
             cell = "%{0}.2f".format(columnwidth) % recall[j]
-            print cell,
-        print
-        print "       F1",
+            print(cell, end=' ')
+        print("%{0}.2f".format(columnwidth) % total_recall)
+        print("       F1", end=' ')
         for j in range(len(labels)):
             cell = "%{0}.2f".format(columnwidth) % F1[j]
-            print cell,
-        print
+            print(cell, end=' ')
+        print("%{0}.2f".format(columnwidth) % total_F1)
     
                   
 def display_adj_cm(
