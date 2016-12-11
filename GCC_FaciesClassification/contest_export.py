12abc47b7ce2ad5453b400457a6a78c52e7fbec9
gccrowther <gccrowther@hotmail.co.uk>
2016-12-11 22:04:38 +0000

    G. Crowther - Facies classifier & results

---
+++ GCC_FaciesClassification/contest_export.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from sklearn.ensemble import ExtraTreesClassifier, VotingClassifier
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import FunctionTransformer
+
+# NOTE: Make sure that the class is labeled 'class' in the data file
+tpot_data = np.recfromcsv('PATH/TO/DATA/FILE', delimiter='COLUMN_SEPARATOR', dtype=np.float64)
+features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpot_data.dtype.names.index('class'), axis=1)
+training_features, testing_features, training_classes, testing_classes = \
+    train_test_split(features, tpot_data['class'], random_state=42)
+
+exported_pipeline = make_pipeline(
+    VarianceThreshold(threshold=0.37),
+    ExtraTreesClassifier(criterion="entropy", max_features=0.71, n_estimators=500)
+)
+
+exported_pipeline.fit(training_features, training_classes)
+results = exported_pipeline.predict(testing_features)
