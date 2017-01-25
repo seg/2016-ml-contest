286bc7c4985daeda9e7b81bf2d60544c53db43b4
Alfredo <alfredo.delafuente.b@gmail.com>
2017-01-25 13:43:14 -0500

    Upload New Submission

---
+++ LA_Team/FinalPipeline.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier, VotingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import FunctionTransformer, MaxAbsScaler
+
+# NOTE: Make sure that the class is labeled 'class' in the data file
+tpot_data = np.recfromcsv('PATH/TO/DATA/FILE', delimiter='COLUMN_SEPARATOR', dtype=np.float64)
+features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpot_data.dtype.names.index('class'), axis=1)
+training_features, testing_features, training_classes, testing_classes = \
+    train_test_split(features, tpot_data['class'], random_state=42)
+
+exported_pipeline = make_pipeline(
+    MaxAbsScaler(),
+    make_union(VotingClassifier([("est", RandomForestClassifier(n_estimators=500))]), FunctionTransformer(lambda X: X)),
+    ExtraTreesClassifier(criterion="entropy", max_features=0.0001, n_estimators=500)
+)
+
+exported_pipeline.fit(training_features, training_classes)
+results = exported_pipeline.predict(testing_features)
